8a6166b8cb0e2b5d7295a6f1f78e654317123905
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-07-17 23:52:57 +0300

    toggling between languages

--- lib/features/settings/ui/settings_view.dart
+++ lib/features/settings/ui/settings_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:small_managements/core/utils/app_colors.dart';
+import 'package:small_managements/features/settings/logic/setting_provider.dart';
 import 'package:small_managements/features/settings/ui/widgets/custom_settings_row.dart';
 import 'package:small_managements/generated/l10n.dart';
 
@@ -9,7 +11,8 @@ class SettingsView extends ConsumerWidget {
   const SettingsView({super.key});
 
   @override
-  Widget build(BuildContext context ,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locale = ref.watch(localizationProvider);
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -42,7 +45,40 @@ class SettingsView extends ConsumerWidget {
               CustomSettingsRow(
                 icon: Icon(Icons.language_outlined),
                 text: S.of(context).language,
-                widget: Text('English'),
+                widget: Container(
+                  width: 100,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: AppColors.kIncreaseContainerColor,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Center(
+                    child: DropdownButton<Locale>(
+                      value: locale,
+                      items: const [
+                        DropdownMenuItem(
+                          value: Locale('en'),
+                          child: Text('English'),
+                        ),
+                        DropdownMenuItem(
+                          value: Locale('ar'),
+                          child: Text('العربية'),
+                        ),
+                      ],
+                      onChanged: (Locale? selected) async {
+                        if (selected != null) {
+                          // غيّر اللغة في Riverpod
+                          ref.read(localizationProvider.notifier).state =
+                              selected;
+
+                          // (اختياري) خزّن اللغة في SharedPreferences
+                          final prefs = await SharedPreferences.getInstance();
+                          await prefs.setString('lang', selected.languageCode);
+                        }
+                      },
+                    ),
+                  ),
+                ),
               ),
               SizedBox(height: 15),
               Text(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:small_managements/core/hive_boxes.dart';
 import 'package:small_managements/core/utils/app_colors.dart';
 import 'package:small_managements/features/products/model/product_model.dart';
@@ -9,17 +10,27 @@ import 'package:small_managements/features/splash/ui/splash_view.dart';
 import 'package:small_managements/generated/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final langCode = prefs.getString('lang') ?? 'en';
+
   await Hive.initFlutter();
   Hive.registerAdapter(ProductModelAdapter());
   await Hive.openBox<ProductModel>(productBox);
-  runApp(ProviderScope(child: const MyApp()));
+  runApp(
+    ProviderScope(
+      overrides: [localizationProvider.overrideWith((ref) => Locale(langCode))],
+
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context,  WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       locale: ref.watch(localizationProvider),
       localizationsDelegates: [
@@ -28,10 +39,7 @@ class MyApp extends ConsumerWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [
-        Locale('ar'),
-        Locale('en'),
-      ],
+      supportedLocales:const [Locale('ar'), Locale('en')],
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark().copyWith(
